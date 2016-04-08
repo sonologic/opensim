39d59e7fa15479f44d3e8f972091ae270b199516
Koen Martens <gmc@sonologic.nl>
2016-04-08 22:19:34 +0200

    RailInfra: refactor rail scanning, move to Layout

--- OpenSim/Addons/RailInfra/RailInfra/Layout.cs
+++ OpenSim/Addons/RailInfra/RailInfra/Layout.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using log4net;
 using System.Reflection;
+using OpenSim.Region.Framework.Scenes;
+using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Addons.RailInfra.Utils;
+using OpenMetaverse;
 
 namespace OpenSim.Addons.RailInfra
 {
@@ -17,17 +21,42 @@ namespace OpenSim.Addons.RailInfra
 		private Dictionary<TrackPoint, int> tp_track_ids;
 
 		private int NextTrackId;
+		private Scene m_scene;
+		private RailInfraModule m_railinfra;
+		private float m_TrackPointDistanceSquared;
+		private float m_TrackPointAngle;
 
-		public Layout ()
+		private void Initialize()
 		{
 			tracks = new Dictionary<int, List<TrackPoint>>();
+
 			tp_track_ids = new Dictionary<TrackPoint, int>();
+
 			NextTrackId = 0;
+		}
+
+		public Layout (RailInfraModule module, Scene scene)
+		{
+			m_scene = scene;
+			m_railinfra = module;
+			m_TrackPointDistanceSquared = 12 * 12;
+			m_TrackPointAngle = 0.16f;
+
+			Initialize ();
+		}
+
+		public Layout (RailInfraModule module, Scene scene, float TrackPointDistanceSquared, float TrackPointAngle)
+		{
+			m_scene = scene;
+			m_railinfra = module;
+			m_TrackPointDistanceSquared = TrackPointDistanceSquared;
+			m_TrackPointAngle = TrackPointAngle;
+
+			Initialize ();
 		}
 
 		private void MergeTracks(int id1, int id2)
 		{
-			
 			int dst;
 			int src;
 			if (id1 < id2) {
@@ -88,6 +117,227 @@ namespace OpenSim.Addons.RailInfra
 				}
 			}
 		}
+
+
+		// track model building:
+
+		private List<SceneObjectGroup> GetGuides()
+		{
+			List<SceneObjectGroup> guides = new List<SceneObjectGroup>();
+
+			List<SceneObjectGroup> objects = m_scene.GetSceneObjectGroups ();
+			m_log.DebugFormat ("[RailInfra]  List length {0}", objects.Count);
+
+			// loop over objects, searching for guides
+			foreach(SceneObjectGroup obj in objects) {
+				if (obj.GetPartCount()==1 && (obj.Name == "Guide" || obj.Name == "Alt Guide")) {
+					guides.Add (obj);
+					m_log.DebugFormat ("[RailInfra]   found: {0} ({1}) at {2}, rot {3}", obj.UUID, obj.Name, obj.AbsolutePosition.ToString(), obj.GroupRotation.ToString());
+				}
+			}
+
+			return guides;
+		}
+
+		private TrackPoint CreateTrackPointFromCandidates(
+			SceneObjectGroup track,
+			TrackPoint candidate_tp,
+			TrackPoint alt_candidate_tp)
+		{
+			// create new TrackPoint descendant (Guide or Switch)
+			TrackPoint new_tp;
+
+			if (candidate_tp != null && alt_candidate_tp != null) {  // g1 is switch
+				m_log.DebugFormat("   | candidate = {0}, alt_candidat = {1}", candidate_tp.ObjectGroup.Description, alt_candidate_tp.ObjectGroup.Description);
+				Switch sw = new Switch (track);
+
+				sw.Branch = alt_candidate_tp;
+				sw.Main = candidate_tp;
+
+				sw.Branch.Prev = sw;
+				sw.Main.Prev = sw;
+
+				new_tp = sw;
+			} else if (candidate_tp != null) {						// g1 is guide to guide
+				Guide guide = new Guide(track);
+				guide.Next = candidate_tp;
+				guide.Next.Prev = guide;
+				new_tp = guide;
+				m_log.DebugFormat ("   | candidate_tp: {0}", candidate_tp);
+				m_log.DebugFormat("   | candidate = {0}, alt_candidat = {1}", candidate_tp.ObjectGroup.Description, "null");
+			} else if (alt_candidate_tp != null) {					// g1 is guide to alt guide
+				Guide guide = new Guide(track);
+				guide.Next = alt_candidate_tp;
+				guide.Next.Prev = guide;
+				new_tp = guide;
+				m_log.DebugFormat("   | candidate = {0}, alt_candidat = {1}", "null", alt_candidate_tp.ObjectGroup.Description);
+			} else {											// g1 is stand-alone
+				m_log.DebugFormat("   | candidate = {0}, alt_candidat = {1}", "null", "null");
+				Guide guide = new Guide(track);
+				guide.Next = null;
+				new_tp = guide;
+			}
+
+			return new_tp;
+		}
+
+		private TrackPoint GetTrackPointFromCache(Dictionary<SceneObjectGroup, TrackPoint> cache, SceneObjectGroup obj)
+		{
+			TrackPoint rv = null;
+
+			if (obj != null) {
+				if (!cache.ContainsKey (obj)) {
+					m_log.DebugFormat ("   | candidate first seen, creating PartialTrackpoint");
+					cache [obj] = new PartialTrackPoint (obj);
+				}
+				rv = cache [obj];
+			}
+
+			return rv;
+		}
+
+		public void ScanScene()
+		{
+			// todo: lock access
+
+			m_log.DebugFormat ("[RailInfra] scanning region {0}", m_scene.Name);
+
+			// collect guides / alt guides
+			List<SceneObjectGroup> guides = GetGuides();
+
+			List<TrackPoint> track_points = new List<TrackPoint> ();
+
+			// this dictionary will get filled one-by-one when looping over
+			// scene objects, entries may be lazily initialized by the
+			// PartialTrackPoint placeholder class
+			Dictionary<SceneObjectGroup, TrackPoint> obj_to_tp = new Dictionary<SceneObjectGroup, TrackPoint> ();
+
+			// loop over guides to fill in links
+			foreach (SceneObjectGroup g1 in guides) {
+				SceneObjectGroup candidate = null;
+				SceneObjectGroup alt_candidate = null;
+
+				m_log.DebugFormat ("out| tp1 = {0}, {1}, {2}, partcount={1}",
+					g1.Description, 
+					g1.AbsolutePosition,
+					StringUtils.FormatAxisAngle(Quaternion.Normalize(g1.GroupRotation)),
+					g1.GetPartCount());
+				
+				foreach (SceneObjectGroup g2 in guides) {
+					if (g1 != g2) {
+						float dist = MathUtils.DistanceSquared(g1, g2);
+						double ang_obj = MathUtils.GetAngle(g1, g2);
+
+
+						if (dist <= m_TrackPointDistanceSquared && ang_obj <= m_TrackPointAngle) {
+							// g2 is inside g1´s scan cone
+							m_log.DebugFormat ("*in| tp2 = {0}, {1}, {2}, distance = {3}, angle = {4}, delta_rot = {5}", 
+								g2.Description, g2.AbsolutePosition, StringUtils.FormatAxisAngle( g2.GroupRotation),
+								dist, ang_obj, StringUtils.FormatAxisAngle(g2.GroupRotation / g1.GroupRotation));
+
+							// if Guide, potential candidate
+							if (g2.Name == "Guide") {
+								if (candidate == null) {
+									candidate = g2;
+								} else {
+									if (MathUtils.DistanceSquared (candidate, g1) > dist) {
+										candidate = g2;
+									}
+								}
+							} else { // Alt Guide, potential alt_candidate
+								if (alt_candidate == null) {
+									alt_candidate = g2;
+								} else {
+									if (MathUtils.DistanceSquared (alt_candidate, g1) > dist) {
+										alt_candidate = g2;
+									}
+								}
+							}
+						} else {
+							// g2 is *not* inside g1´s scan cone
+							m_log.DebugFormat (" in| tp2 = {0}, {1}, {2}, distance = {3}, angle = {4}", 
+								g2.Description, g2.AbsolutePosition, StringUtils.FormatAxisAngle (g2.GroupRotation),
+								dist, ang_obj);
+						}
+					}
+				}
+
+				// if g1 itself is alt guide, don't consider promotion to switch
+				if (g1.Name == "Alt Guide" && candidate != null && alt_candidate != null) {
+					candidate = null;
+				}
+					
+				// get TrackPoint objects from obj_to_tp dict (create if first seen)
+				TrackPoint candidate_tp = GetTrackPointFromCache(obj_to_tp, candidate);
+
+				// todo: check if alt_candidate angle is not unexpected (ie, not an 
+				// alt guide coming in on the direction of this guide), otherwise reject
+				// alt_candidate
+				TrackPoint alt_candidate_tp = GetTrackPointFromCache(obj_to_tp, alt_candidate);
+
+				// create new TrackPoint descendant (Guide or Switch)
+				TrackPoint new_tp = CreateTrackPointFromCandidates(g1, candidate_tp, alt_candidate_tp);
+
+				if (obj_to_tp.ContainsKey (g1) && (obj_to_tp [g1].GetType () == typeof(PartialTrackPoint))) {
+					m_log.DebugFormat ("   | already present in obj_to_tp, replace");
+					m_log.DebugFormat ("   |   old in table {0}", obj_to_tp [g1]);
+					m_log.DebugFormat ("   |   new_tp       {0}", new_tp);
+
+					// someone inserted g1 as PartialTrackPoint, replace
+					TrackPoint partial_track_point = obj_to_tp [g1];
+
+					new_tp.Prev = obj_to_tp [g1].Prev;
+					obj_to_tp [g1] = new_tp;
+
+					// replace all references to Partial
+					foreach (TrackPoint tp in obj_to_tp.Values) {
+						tp.ReplaceLink (partial_track_point, new_tp);
+					}
+
+					m_log.DebugFormat ("   |   after repl   {0}", obj_to_tp [g1]);
+				} else {
+					obj_to_tp [g1] = new_tp;
+				}
+
+				track_points.Add (new_tp);
+			} // foreach (SceneObjectGroup g2 in guides)
+
+			m_log.DebugFormat ("   | Before resolving:");
+			foreach (TrackPoint tp in track_points) {
+				m_log.DebugFormat ("   |   {0}", tp);
+			}
+
+			// resolve any links to PartialTrackPoint's
+			foreach (TrackPoint tp in track_points) {
+				if (tp.GetType () == typeof(Guide)) {
+					Guide g = (Guide)tp;
+					if (g.Next != null && g.Next.GetType () == typeof(PartialTrackPoint))
+						g.Next = obj_to_tp [g.Next.ObjectGroup];
+				} else {
+					Switch s = (Switch)tp;
+					if (s.Main != null && s.Main.GetType () == typeof(PartialTrackPoint))
+						s.Main = obj_to_tp [s.Main.ObjectGroup];
+					if (s.Branch != null && s.Branch.GetType () == typeof(PartialTrackPoint))
+						s.Branch = obj_to_tp [s.Branch.ObjectGroup];
+				}
+			}
+
+			// now we have Next and Prev initialised for each TrackPoint
+
+			// one-by-one add to layout, this will seperate the tp's in disconnected graphs
+
+			foreach (TrackPoint tp in track_points) {
+				m_log.DebugFormat ("add to layout: {0}", tp);
+				Add (tp);					
+			}
+
+			m_log.Debug (ToString ());
+
+		} // ScanScene()
+
+
+
+		// string conversions:
 
 		public override string ToString()
 		{
--- OpenSim/Addons/RailInfra/RailInfraRegionModule.cs
+++ OpenSim/Addons/RailInfra/RailInfraRegionModule.cs
@@ -30,10 +30,10 @@ namespace OpenSim.Addons.RailInfra
 		private ScriptChatHandler m_chat_handler;
 
 		// config values
-		private string m_ManagerUUID;
+		public string m_ManagerUUID { get; private set; }
 		public int m_channel { get; private set; }
-		private float m_TrackPointDistanceSquared;
-		private float m_TrackPointAngle;
+		public float m_TrackPointDistanceSquared { get; private set; }
+		public float m_TrackPointAngle { get; private set; }
 
 		// internal book-keeping
 		public Dictionary<Scene, Layout> m_layouts { get; private set; }
@@ -101,12 +101,13 @@ namespace OpenSim.Addons.RailInfra
 			scene.EventManager.OnChatFromWorld -= m_chat_handler.ProcessChat;
 			//scene.EventManager.OnChatBroadcast -= OnChatFromClient;
 		}
-
+			
 		public void PostInitialise()
 		{
 			m_log.DebugFormat("[RailInfra] PostInitialise()");
 
 			m_log.DebugFormat ("[RailInfra] Scenes:");
+
 			ScanScenes ();
 		}
 
@@ -127,277 +128,17 @@ namespace OpenSim.Addons.RailInfra
 
 		// helpers:
 
+		// <summary>Scan (build layout model) all scenes in m_scenes</summary>
+		//
 		public void ScanScenes()
 		{
-			// todo: lock access
 			m_layouts = new Dictionary<Scene, Layout>();
 
 			foreach (Scene scene in m_scenes) {
-				
-
-				m_log.DebugFormat ("[RailInfra] scanning region {0}", scene.Name);
-
-				IScriptModule[] engines = scene.RequestModuleInterfaces<IScriptModule>();
-				foreach (IScriptModule engine in engines) {
-					m_log.DebugFormat ("[RailInfra] script engine: {0}", engine.Name);
-				}
-				
-				// collect guides / alt guides
-				List<SceneObjectGroup> objects = scene.GetSceneObjectGroups ();
-				List<SceneObjectGroup> guides = new List<SceneObjectGroup>();
-				m_log.DebugFormat ("[RailInfra]  List length {0}", objects.Count);
-
-				// loop over objects, searching for guides
-				foreach(SceneObjectGroup obj in objects) {
-
-					if (obj.GetPartCount()==1 && (obj.Name == "Guide" || obj.Name == "Alt Guide")) {
-						guides.Add (obj);
-						m_log.DebugFormat ("[RailInfra]   found: {0} ({1}) at {2}, rot {3}", obj.UUID, obj.Name, obj.AbsolutePosition.ToString(), obj.GroupRotation.ToString());
-					}
-				}
-
-				List<TrackPoint> track_points = new List<TrackPoint> ();
-				Dictionary<SceneObjectGroup, TrackPoint> obj_to_tp = new Dictionary<SceneObjectGroup, TrackPoint> ();
-
-				// loop over guides to fill in links
-				foreach (SceneObjectGroup g1 in guides) {
-					SceneObjectGroup candidate = null;
-					SceneObjectGroup alt_candidate = null;
-
-
-					m_log.DebugFormat ("out| tp1 = {0}, {1}, {2}, partcount={1}",
-						g1.Description, 
-						g1.AbsolutePosition,
-						StringUtils.FormatAxisAngle(Quaternion.Normalize(g1.GroupRotation)),
-						g1.GetPartCount());
-					foreach (SceneObjectGroup g2 in guides) {
-						float dist = MathUtils.DistanceSquared(g1, g2);
-
-						if (g1 != g2) {
-							double ang_obj = MathUtils.GetAngle(g1, g2);
-
-							if (dist <= m_TrackPointDistanceSquared && ang_obj <= m_TrackPointAngle) {
-								m_log.DebugFormat ("*in| tp2 = {0}, {1}, {2}, distance = {3}, angle = {4}, delta_rot = {5}", 
-									g2.Description, g2.AbsolutePosition, StringUtils.FormatAxisAngle( g2.GroupRotation),
-									dist, ang_obj, StringUtils.FormatAxisAngle(g2.GroupRotation / g1.GroupRotation));
-
-								// if Guide, potential candidate
-								if (g2.Name == "Guide") {
-									if (candidate == null) {
-										candidate = g2;
-									} else {
-										if (MathUtils.DistanceSquared (candidate, g1) > dist) {
-											candidate = g2;
-										}
-									}
-								} else { // Alt Guide, potential alt_candidate
-									if (alt_candidate == null) {
-										alt_candidate = g2;
-									} else {
-										if (MathUtils.DistanceSquared (alt_candidate, g1) > dist) {
-											alt_candidate = g2;
-										}
-									}
-								}
-							} else {
-								m_log.DebugFormat (" in| tp2 = {0}, {1}, {2}, distance = {3}, angle = {4}", 
-									g2.Description, g2.AbsolutePosition, StringUtils.FormatAxisAngle (g2.GroupRotation),
-									dist, ang_obj);
-							}
-						}
-					}
-
-					TrackPoint new_tp;
-
-					// if g1 itself is alt guide, don't consider promotion to switch
-					if (g1.Name == "Alt Guide" && candidate != null && alt_candidate != null) {
-						candidate = null;
-					}
-
-
-					// get TrackPoint objects from obj_to_tp dict (create if first seen)
-					TrackPoint candidate_tp = null;
-					TrackPoint alt_candidate_tp = null;
-
-					if (candidate != null) {
-						if (!obj_to_tp.ContainsKey (candidate)) {
-							m_log.DebugFormat ("   | candidate first seen, creating PartialTrackpoint");
-							obj_to_tp [candidate] = new PartialTrackPoint (candidate);
-						}
-						candidate_tp = obj_to_tp [candidate];
-					}
-
-					// todo: check if alt_candidate angle is not unexpected (ie, not an 
-					// alt guide coming in on the direction of this guide), otherwise reject
-					// alt_candidate
-					if (alt_candidate != null) {
-						if (!obj_to_tp.ContainsKey (alt_candidate)) {
-							m_log.DebugFormat ("   | alt_candidate first seen, creating PartialTrackpoint");
-							obj_to_tp [alt_candidate] = new PartialTrackPoint (alt_candidate);
-						}
-						alt_candidate_tp = obj_to_tp [alt_candidate];
-
-						m_log.DebugFormat("   | my  pos = {0}, rot = {1}", g1.AbsolutePosition, StringUtils.FormatAxisAngle(g1.GroupRotation));
-						m_log.DebugFormat("   | alt pos = {0}, rot = {1}", alt_candidate.AbsolutePosition, StringUtils.FormatAxisAngle(alt_candidate.GroupRotation));	
-					}
-
-					if (candidate != null && alt_candidate != null) {  // g1 is switch
-						m_log.DebugFormat("   | candidate = {0}, alt_candidat = {1}", candidate.Description, alt_candidate.Description);
-						Switch sw = new Switch (g1);
-
-						sw.Branch = alt_candidate_tp;
-						sw.Main = candidate_tp;
-
-						sw.Branch.Prev = sw;
-						sw.Main.Prev = sw;
-
-						new_tp = sw;
-					} else if (candidate != null) {						// g1 is guide to guide
-						Guide guide = new Guide(g1);
-						guide.Next = candidate_tp;
-						guide.Next.Prev = guide;
-						new_tp = guide;
-						m_log.DebugFormat ("   | candidate_tp: {0}", candidate_tp);
-						m_log.DebugFormat("   | candidate = {0}, alt_candidat = {1}", candidate.Description, "null");
-					} else if (alt_candidate != null) {					// g1 is guide to alt guide
-						Guide guide = new Guide(g1);
-						guide.Next = alt_candidate_tp;
-						guide.Next.Prev = guide;
-						new_tp = guide;
-						m_log.DebugFormat("   | candidate = {0}, alt_candidat = {1}", "null", alt_candidate.Description);
-					} else {											// g1 is stand-alone
-						m_log.DebugFormat("   | candidate = {0}, alt_candidat = {1}", "null", "null");
-						Guide guide = new Guide(g1);
-						guide.Next = null;
-						new_tp = guide;
-					}
-
-					if (obj_to_tp.ContainsKey (g1) && (obj_to_tp [g1].GetType () == typeof(PartialTrackPoint))) {
-						m_log.DebugFormat ("   | already present in obj_to_tp, replace");
-						m_log.DebugFormat ("   |   old in table {0}", obj_to_tp [g1]);
-						m_log.DebugFormat ("   |   new_tp       {0}", new_tp);
-
-						// someone inserted g1 as PartialTrackPoint, replace
-						TrackPoint partial_track_point = obj_to_tp [g1];
-
-						new_tp.Prev = obj_to_tp [g1].Prev;
-						obj_to_tp [g1] = new_tp;
-
-						// replace all references to Partial
-						foreach (TrackPoint tp in obj_to_tp.Values) {
-							tp.ReplaceLink (partial_track_point, new_tp);
-						}
-
-						m_log.DebugFormat ("   |   after repl   {0}", obj_to_tp [g1]);
-					} else {
-						obj_to_tp [g1] = new_tp;
-					}
-
-					track_points.Add (new_tp);
-
-					/*
-					tp1.Next = candidate;
-					if (candidate != null) {
-						candidate.Prev = tp1;
-						m_log.DebugFormat ("------ tp1 ({0}) next = {1}", tp1.ObjectGroup.UUID, tp1.Next.ObjectGroup.UUID);
-					} else {
-						m_log.DebugFormat ("------ tp1 ({0}) next = null", tp1.ObjectGroup.UUID);
-					}*/
-
-				}
-
-				m_log.DebugFormat ("   | Before resolving:");
-				foreach (TrackPoint tp in track_points) {
-					m_log.DebugFormat ("   |   {0}", tp);
-				}
-				// resolve any links to PartialTrackPoint's
-				foreach (TrackPoint tp in track_points) {
-					if (tp.GetType () == typeof(Guide)) {
-						Guide g = (Guide)tp;
-						if (g.Next != null && g.Next.GetType () == typeof(PartialTrackPoint))
-							g.Next = obj_to_tp [g.Next.ObjectGroup];
-					} else {
-						Switch s = (Switch)tp;
-						if (s.Main != null && s.Main.GetType () == typeof(PartialTrackPoint))
-							s.Main = obj_to_tp [s.Main.ObjectGroup];
-						if (s.Branch != null && s.Branch.GetType () == typeof(PartialTrackPoint))
-							s.Branch = obj_to_tp [s.Branch.ObjectGroup];
-					}
-				}
-
-				// now we have Next and Prev initialised for each TrackPoint
-
-				// one-by-one add to layout, this will seperate the tp's in disconnected graphs
-				m_layouts[scene] = new Layout();
-				foreach (TrackPoint tp in track_points) {
-					m_log.DebugFormat ("add to layout: {0}", tp);
-					m_layouts[scene].Add (tp);					
-				}
-
-				m_log.Debug (m_layouts[scene].ToString ());
+				m_layouts [scene] = new Layout (this, scene);
+				m_layouts [scene].ScanScene ();
 			}
-						
 		}
-
-		/*
-		private void OnChatFromClient(Object sender_obj, OSChatMessage chat)
-		{
-			//if (chat.Channel == Channel) {
-
-			m_log.DebugFormat ("[RailInfra] chat from {0} ({3}) on channel {2}, \"{1}\"",
-				chat.SenderUUID.ToString(), 
-				chat.Message,
-				chat.Channel,
-				chat.From
-			);
-
-			SceneObjectGroup sender = ((Scene)chat.Scene).GetSceneObjectGroup(chat.SenderUUID);
-
-			if (sender != null) {
-				m_log.DebugFormat ("sender object group {0} ({1})", sender.AbsolutePosition.ToString (), sender.Name);
-
-				if (chat.Channel == m_channel) {
-					string[] tokens = chat.Message.Split (' ');
-
-					if (tokens.Length > 1) {
-						string cmd = tokens [0];
-
-						m_log.DebugFormat ("cmd is {0}", cmd);
-
-						switch (cmd) {
-						case "register":
-							m_log.DebugFormat ("registering {0}", sender.UUID);
-							if (!m_fleet.ContainsUUID (sender.UUID)) {
-								m_fleet.RegisterVehicle (sender.UUID, sender);
-							}
-							break;
-						default:
-							break;
-						}
-
-						IScriptModule[] engines = sender.Scene.RequestModuleInterfaces<IScriptModule>();
-						foreach (IScriptModule engine in engines) {
-							m_log.DebugFormat ("[RailInfra] notifying script engine: {0}", engine.Name);
-
-							object[] resobj = new object[]
-							{
-								// Event: link_message( integer sender_num, integer num, string str, key id ){ ; }
-								new LSL_Types.LSLInteger(0), new LSL_Types.LSLInteger(42), new LSL_Types.LSLString("foo bar"), new LSL_Types.LSLString("")
-							};
-
-							engine.PostObjectEvent(sender.UUID, //partItemID,
-								"link_message",
-								resobj);
-						}
-
-					} else {
-						m_log.DebugFormat ("Invalid number of tokens: {0}", tokens.Length);
-					}
-				}
-			}
-
-			//}
-		}*/
 	}
 }
 
